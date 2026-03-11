f1dd5b12f08525097e7c9bc5d863b99dbba16917
leolinder-hash <leolinder074@gmail.com>
2026-03-11 16:18:36 +0100

    comment away destructive JS. remove unused css

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ document.write("<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1>")
 document.write("<p>Another document.write() call</p>")
 
 // Förhindrar användaren att navigera bakåt
-history.pushState(null, "", location.href)
-window.onpopstate = () => history.go(1)
+/* history.pushState(null, "", location.href)
+window.onpopstate = () => history.go(1) */
 
 // Ändrar bakgrundsfärgen slumpmässigt var 500ms
-setInterval(() => {
+/* setInterval(() => {
   document.body.style.backgroundColor =
     "#" + Math.floor(Math.random() * 16777215).toString(16)
-}, 500)
+}, 500) */
 
 // Visar innehållet på sidan efter 5 sekunder
-setTimeout(() => {
+/* setTimeout(() => {
   document.body.style.display = "block"
-}, 5000)
+}, 5000) */
 
 // Loggar 10000 tal till konsolen efter 1 sekund
-setTimeout(() => {
+/* setTimeout(() => {
   for (let i = 0; i < 10000; i++) {
     console.log(i)
   }
-}, 1000)
+}, 1000) */
 
 // // Visar en alert när sidan har laddats
 // document.addEventListener("DOMContentLoaded", function () {
@@ -36,6 +36,7 @@ setTimeout(() => {
 // })
 
 // Loggar 100 000 siffror direkt när sidan laddas
-for (let i = 0; i < 100000; i++) {
-  console.log(i)
-}
+/* for (let i = 0; i < 100000; i++) {
+  console.log(i) */
+/* }
+ */
